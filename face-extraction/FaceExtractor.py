459600281090614275e86dbef89571dec640afe7
visatanna <vinitsantanna@gmail.com>
2019-06-21 03:16:11 -0300

    refactoring face_extraction and face detection tuning

--- face-extraction/FaceExtractor.py
+++ face-extraction/FaceExtractor.py
@@ -1,19 +1,77 @@
 import cv2
 import os
+from pathlib import Path
 import matplotlib.image
+from entities.face_pair import FacePair
+from entities.face import Face
+from enumarators.match_type import MatchType
+import pdb
 
+def save_all_pairs(list_all_pairs):
+    main_path = Path('../all_cropped_faces/')
+    if not os.path.exists(main_path):
+        os.mkdir(main_path)
 
+    for match_type in MatchType:
+        type_path = main_path / match_type.value
+        if not os.path.exists(type_path):
+            os.mkdir(type_path)
+        pairs_list = list(filter(lambda x: x.match_type == match_type ,list_all_pairs ))
+
+        for pair in pairs_list:
+            pair_folder_path = type_path / pair.origin_folder_name
+            if not os.path.exists(pair_folder_path):
+                os.mkdir(pair_folder_path)
+            for face in pair.faces:
+                matplotlib.image.imsave(pair_folder_path / face.image_name , face.face_image)
+                print("generating cropped image in " + str(pair_folder_path / face.image_name))
+
+def extract_faces_from_folder( folder_name , face_classifier, match_type):
+    all_pairs = [] 
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+
+    match_path = Path(folder_name)
+    for  directory in os.listdir(folder_name):
+        failed_to_find_face = False
+
+        pair_path = match_path /  str(directory)
+        detected_faces = [] 
+        for image_name in os.listdir(pair_path):
+            full_image = cv2.imread(str(pair_path / image_name))
+            face_detected_img , face = detect_face(face_classifier, full_image)
+            processed_img = None
+            if len(face) == 0 or failed_to_find_face:
+                failed_to_find_face = True
+                processed_img = full_image
+            else:
+                processed_img = crop_image_by_face(full_image, face)
+            face_img = cv2.cvtColor(processed_img, cv2.COLOR_BGR2RGB)
+            detected_faces.append(Face(face_img , image_name))
+               
+        face_pair = None
+
+        if failed_to_find_face:
+            face_pair = FacePair(detected_faces[0] , detected_faces[1], MatchType.FACE_NOT_FOUND,  directory+"_"+match_type.value )
+        else:
+            face_pair = FacePair(detected_faces[0] , detected_faces[1], match_type ,  directory)
+        
+        all_pairs.append(face_pair)
+
+    return all_pairs
+    
 # Returns the default frontal face classifier from cv2
 def face_classifier():
-    return cv2.CascadeClassifier('D:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt2.xml')
+    current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+    return cv2.CascadeClassifier(str(current_path / "opencv_sources/haarcascade_frontalface_alt2.xml"))
 
 
 # Viola Jones Classifier code (https://www.superdatascience.com/blogs/opencv-face-detection)
 def detect_face(f_cascade, colored_img, scale_factor=1.05):
     img_copy = colored_img.copy()
     gray_img = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
-    gray_equalized = cv2.equalizeHist(gray_img)
-    faces = f_cascade.detectMultiScale(gray_equalized, scaleFactor=scale_factor, minNeighbors=4)
+    #gray_equalized = cv2.equalizeHist(gray_img)
+    faces = f_cascade.detectMultiScale(gray_img, scaleFactor=scale_factor, minNeighbors=1 , minSize =(60,60))
     for (x, y, w, h) in faces:
         cv2.rectangle(img_copy, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return img_copy, faces
@@ -34,101 +92,13 @@ def show_images(images):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
-# Initialize classifier
-face_cascade = face_classifier()
-
-matches_path = 'matches'
-if not os.path.isdir(matches_path):
-    os.mkdir(matches_path)
-
-match_unknown_faces_number = 0
-match_unknown_faces = []
-match_pair = 0
-match_path = '..\\scrapper\\matches'
-for directory in range(3000):
-    match_pair += 1
-
-    match_pair_path = match_path + '\\' + str(directory)
-    pair_files = os.listdir(match_pair_path)
-    match_one_img = cv2.imread(match_pair_path + '\\' + pair_files[0])
-    match_two_img = cv2.imread(match_pair_path + '\\' + pair_files[1])
-
-    face_detected_img_one, face_one = detect_face(face_cascade, match_one_img)
-    face_detected_img_two, face_two = detect_face(face_cascade, match_two_img)
-
-    if len(face_one) == 0 or len(face_two) == 0:
-        match_unknown_faces_number += 1
-        match_unknown_faces.append(match_pair)
-        continue
-
-    face_cropped_img_one = crop_image_by_face(match_one_img, face_one)
-    face_cropped_img_two = crop_image_by_face(match_two_img, face_two)
-
-    match_folder = matches_path + '/' + str(match_pair)
-    if not os.path.isdir(match_folder):
-        os.mkdir(match_folder)
-
-    rgb_img_one = cv2.cvtColor(face_cropped_img_one, cv2.COLOR_BGR2RGB)
-    rgb_img_two = cv2.cvtColor(face_cropped_img_two, cv2.COLOR_BGR2RGB)
-
-    matplotlib.image.imsave(match_folder + '/' + pair_files[0], rgb_img_one)
-    matplotlib.image.imsave(match_folder + '/' + pair_files[1], rgb_img_two)
-
-    print("saved matched " + str(match_pair))
-
-
-mismatches_path = 'mismatches'
-if not os.path.isdir(mismatches_path):
-    os.mkdir(mismatches_path)
-
-
-mismatch_unknown_faces_number = 0
-mismatch_unknown_faces = []
-mismatch_pair = 0
-mismatch_path = '..\\scrapper\\mismatches'
-for directory in range(3000):
-    mismatch_pair += 1
-
-    mismatch_pair_path = mismatch_path + '\\' + str(directory)
-    pair_files = os.listdir(mismatch_pair_path)
-    mismatch_one_img = cv2.imread(mismatch_pair_path + '\\' + pair_files[0])
-    mismatch_two_img = cv2.imread(mismatch_pair_path + '\\' + pair_files[1])
-
-    face_detected_img_one, face_one = detect_face(face_cascade, mismatch_one_img)
-    face_detected_img_two, face_two = detect_face(face_cascade, mismatch_two_img)
-
-    if len(face_one) == 0 or len(face_two) == 0:
-        mismatch_unknown_faces_number += 1
-        mismatch_unknown_faces.append(mismatch_pair)
-        continue
-
-    face_cropped_img_one = crop_image_by_face(mismatch_one_img, face_one)
-    face_cropped_img_two = crop_image_by_face(mismatch_two_img, face_two)
-
-    mismatch_folder = mismatches_path + '/' + str(mismatch_pair)
-    if not os.path.isdir(mismatch_folder):
-        os.mkdir(mismatch_folder)
-
-    rgb_img_one = cv2.cvtColor(face_cropped_img_one, cv2.COLOR_BGR2RGB)
-    rgb_img_two = cv2.cvtColor(face_cropped_img_two, cv2.COLOR_BGR2RGB)
-
-    matplotlib.image.imsave(mismatch_folder + '/' + pair_files[0], rgb_img_one)
-    matplotlib.image.imsave(mismatch_folder + '/' + pair_files[1], rgb_img_two)
-
-    print("saved mismatched " + str(mismatch_pair))
-
-
-print("Match faces unrecognized: " + str(match_unknown_faces_number))
-print(match_unknown_faces)
-
-print("Mismatch faces unrecognized: " + str(mismatch_unknown_faces_number))
-print(mismatch_unknown_faces)
-
-with open('faces_not_recognized.txt', 'a') as file:
-    file.write('match: ')
-    for item in match_unknown_faces:
-        file.write("%d " % item)
-    file.write('\nmismatch: ')
-    for item in mismatch_unknown_faces:
-        file.write("%d " % item)
+if __name__ == "__main__":    
+    # Initialize classifier
+    face_cascade = face_classifier()
+    all_pairs = []
+    match_path = Path('../scrapper/matches')
+    mismatch_path = Path('../scrapper/mismatches')
+    all_pairs.extend(extract_faces_from_folder(match_path , face_cascade, MatchType.MATCH))
+    all_pairs.extend(extract_faces_from_folder(mismatch_path , face_cascade, MatchType.MISMATCH))
+    save_all_pairs(all_pairs)
+    
---
+++ face-extraction/entities/face.py
@@ -0,0 +1,4 @@
+class Face( ):
+    def __init__(self, face_image , image_name):
+        self.face_image = face_image 
+        self.image_name = image_name
